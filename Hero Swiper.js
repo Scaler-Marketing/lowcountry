c062523b4eba945099311afa725c81e135971ddc
jesse-shauffer <111093802+jesse-shauffer@users.noreply.github.com>
2024-12-14 16:40:08 -0500

    Updating Hero Swiper

---
+++ Hero Swiper.js
@@ -0,0 +1,51 @@
+$(".slider-main_component").each(function (index) {
+  let loopMode = true;
+  if ($(this).attr("loop-mode") === "true") {
+    loopMode = true;
+  }
+  let sliderDuration = 1000;
+  if ($(this).attr("slider-duration") !== undefined) {
+    sliderDuration = +$(this).attr("slider-duration");
+  }
+  const swiper = new Swiper($(this).find(".swiper")[0], {
+    speed: sliderDuration,
+    loop: loopMode,
+    autoplay: { enabled: true, delay: 5000 },
+    autoHeight: false,
+    centeredSlides: false,
+    followFinger: true,
+    freeMode: false,
+    slideToClickedSlide: false,
+    slidesPerView: 1,
+    spaceBetween: 0,
+    rewind: false,
+    grabCursor: true,
+    mousewheel: {
+      forceToAxis: true
+    },
+    keyboard: {
+      enabled: false,
+      onlyInViewport: true
+    },
+    pagination: {
+      el: $(this).find(".swiper-bullet-wrapper")[0],
+      bulletActiveClass: "is-active",
+      bulletClass: "swiper-bullet",
+      bulletElement: "div",
+      clickable: true
+    },
+    navigation: {
+      nextEl: $(this).find(".swiper-next")[0],
+      prevEl: $(this).find(".swiper-prev")[0],
+      disabledClass: "is-disabled"
+    },
+    scrollbar: {
+      el: $(this).find(".swiper-drag-wrapper")[0],
+      draggable: true,
+      dragClass: "swiper-drag",
+      snapOnRelease: true
+    },
+    slideActiveClass: "is-active",
+    slideDuplicateActiveClass: "is-active"
+  });
+});
