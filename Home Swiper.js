a4f6a67e5da6b8234c19a4336e416d32d616a27d
jesse-shauffer <111093802+jesse-shauffer@users.noreply.github.com>
2024-12-14 16:40:11 -0500

    Updating Home Swiper

---
+++ Home Swiper.js
@@ -0,0 +1,9 @@
+  const gallerySlider = new Swiper(".swiper.is-gallery", {
+    // Parameters
+    loop: true,
+    slidesPerView: 2,
+    centeredSlides: true,
+    speed: 800,
+    grabCursor: true,
+    parallax: true
+  });
