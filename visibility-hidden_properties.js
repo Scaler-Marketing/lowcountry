8f084e5d072f030be8341f736b0c9a69339e1320
jesse-shauffer <jesse@scalermarketing.com>
2025-01-06 21:28:50 -0500

    Create visibility-hidden_properties.js

---
+++ visibility-hidden_properties.js
@@ -0,0 +1,5 @@
+document.querySelectorAll('.property-item').forEach(item => {
+  if (getComputedStyle(item).display === 'none') {
+    item.remove(); // Remove the hidden item from the DOM
+  }
+});
